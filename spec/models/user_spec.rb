76d721ed320c54f5fcca15f16a20dc877e0c951f
RhysWood <rhyswoody@gmail.com>
2022-04-27 01:14:19 +0000

    add password and password_confirmation match test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
     it 'should be created with the required fields' do
       @user = User.new({
-        name: 'John',
+        name: "John",
         email: "email@email.com",
         password: "password",
         password_confirmation: "password"
@@ -12,10 +12,21 @@ RSpec.describe User, type: :model do
       expect(@user).to be_valid
     end
 
+    it 'should match password and password_confirmation' do
+      @user = User.new({
+        name: "John",
+        email: "email@email.com",
+        password: "password",
+        password_confirmation: "password1"
+      })
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+    end
+
   end
 
   describe '.authenticate_with_credentials' do
-    # examples for this class method here
+    # exam
   end
 
 end
